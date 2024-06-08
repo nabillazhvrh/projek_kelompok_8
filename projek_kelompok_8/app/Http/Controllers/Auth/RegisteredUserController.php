668ca304b49b88e0012f79a53d4c5c2f28a00f0a
khairulhalis <kholissugara@gmail.com>
2024-06-08 22:30:49 +0700

    perubahan Register

--- projek_kelompok_8/app/Http/Controllers/Auth/RegisteredUserController.php
+++ projek_kelompok_8/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,6 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
